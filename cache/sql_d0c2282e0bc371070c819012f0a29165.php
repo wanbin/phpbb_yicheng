b0d371425bcad319bd247a83a2818ea2a4ea9774
wanbin <wanbin@sevenga.com>
2015-08-08 16:45:05 +0800

    de ru

--- cache/sql_d0c2282e0bc371070c819012f0a29165.php
+++ cache/sql_d0c2282e0bc371070c819012f0a29165.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1439023187
+1439023985
 SELECT forum_id, forum_name, parent_id, forum_type, forum_flags, forum_options, left_id, right_id FROM phpbb_forums ORDER BY left_id ASC
 229
 a:1:{i:0;a:8:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:12:"坦克风云";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"1";s:11:"forum_flags";s:2:"48";s:13:"forum_options";s:1:"0";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"2";}}
